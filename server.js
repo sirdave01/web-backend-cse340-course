474092eb5657f2131e184b4dea78221eaf456920
sirdave01 <d08178084956@gmail.com>
2026-05-04 14:12:53 -0600

    routing with Expres JS continues

--- server.js
+++ server.js
@@ -2,21 +2,51 @@
 
 import express from 'express';
 
+import { fileURLToPath } from 'url';
+
+import path from 'path';
+
 // after the .env file is created we'll modify the server.js file to use the environment
 // variables instead of hardcoding the values
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// creating the __filename and __dirname variables to be used in the server.js file to get
+// the current file name and directory name
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
-});
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const app = express();
 
 app.listen(PORT, () => {
 
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
 
     console.log(`Environment: ${NODE_ENV}`);
+});
+
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+  * Routes
+  */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
 });
