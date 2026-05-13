cf11b1903e2cbc4b26c8c63173e6c4600ec82c3d
sirdave01 <d08178084956@gmail.com>
2026-05-13 07:58:52 -0600

    fixed the display issue of the organization's name on the projects page from the database using te SELECT query statement

--- src/models/projects.js
+++ src/models/projects.js
@@ -6,8 +6,17 @@ import db from './db.js';
 const getAllProjects = async () => {
 
     const query = `
-        SELECT project_id, organization_id, title, description, location, project_date
-        FROM projects;
+        SELECT
+        p.project_id,
+        p.title,
+        p.description,
+        p.location,
+        p.project_date,
+        o.name AS organization_name
+
+        FROM projects p
+        JOIN organizations o ON p.organization_id = o.organization_id
+        ORDER BY p.project_date DESC;
     `;
 
     const result = await db.query(query);
