34e6c55e8eed1418e9242856bef30fffbc84fa16
sirdave01 <d08178084956@gmail.com>
2026-06-03 08:18:35 -0600

    adding new function in the categories.ejs in the controllers directory

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -48,6 +48,34 @@ const showCategoryDetailsPage = async (req, res) => {
         projects 
     });
 
- };
+};
+ 
+// Create a new function showAssignCategoriesForm.
+// Get the projectId from the request parameters.
+// This function should retrieve the project details using the existing getProjectDetails model function.
+// (You will likely need to add this to your list of imports from the project model file).
+// It should also retrieve all categories using the existing getAllCategories model function.
+// Additionally, it should retrieve the categories currently assigned to the project using the existing
+// getCategoriesByServiceProjectId model function. (You will likely need to add this to your list of imports
+// from the categories model file.)
+// It should set the title variable to be, "Assign Categories to Project"
+// Finally, it should render a view assign-categories (to be created in the next step)
+// and pass the project details, all categories, and the assigned categories to the view.
 
-export { showCategoriesPage, showCategoryDetailsPage };
+const showAssignCategoriesForm = async (req, res) => { };
+
+
+// Create a new function processAssignCategoriesForm.
+// Get the projectId from the request parameters.
+// Get the selected category IDs from the request body.
+// (Assume the form field name is categories and it will be an array of selected category IDs.)
+// This function should call the updateCategoryAssignments model function you created in the previous step,
+// passing in the projectId and the array of selected category IDs. (You will likely need to add this to your
+// list of imports from the categories model file.)
+// Set a success flash message.
+// Redirect the user back to the project details page /project/{projectId}.
+
+const processAssignCategoriesForm = async (req, res) => { };
+
+
+export { showCategoriesPage, showCategoryDetailsPage, showAssignCategoriesForm, processAssignCategoriesForm };
